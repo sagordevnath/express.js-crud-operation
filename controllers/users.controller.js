9cbfd10f1f1c583f92b815e3fe8730171916540e
sagor devnath <ssagor64@gmail.com>
2022-09-03 21:03:54 +0600

    get all user create

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -26,3 +26,30 @@ controller.getRandomUser = (req, res, next) => {
     }
   });
 };
+
+controller.getAllUsers = (req, res, next) => {
+    const { limit } = req.query;
+    data.read("users", "users", (err, users) => {
+      if (!err && Array.isArray(users) && users.length > 0) {
+        if (limit) {
+          const limitedUsers = users.slice(0, Number(limit));
+          res.status(200).json({
+            success: true,
+            message: "Limited Users",
+            limitedUsers,
+          });
+        } else {
+          res.status(200).json({
+            success: true,
+            message: "All Users",
+            users,
+          });
+        }
+      } else {
+        res.status(500).json({
+          success: false,
+          message: "Internal server error. No users found",
+        });
+      }
+    });
+  };
